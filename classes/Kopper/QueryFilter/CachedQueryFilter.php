97b788e5fa694ac5968d3eaf2cf9a902787071a8
Ben Connito <benconnito@gmail.com>
2015-09-11 11:51:22 -0400

    include maxPerPage in cached query

--- classes/Kopper/QueryFilter/CachedQueryFilter.php
+++ classes/Kopper/QueryFilter/CachedQueryFilter.php
@@ -9,14 +9,14 @@ class CachedQueryFilter extends QueryFilter {
 
   public function paginate(ModelCriteria $query, $page = 1, $maxPerPage = 25) {
     $queryMd5 = md5($query->toString());
-    $key = $query->getTableMap()->getName() . '_query_' . $queryMd5 . '_' . $page;
+    $key = $query->getTableMap()->getName() . '_query_' . $queryMd5 . '_' . $page . '_' . $maxPerPage;
 
     $records = GlobalCache::get($key);
 
-    if (empty($records) === true) {      
+    if (empty($records) === true) {
       $records = $query->paginate($page, $maxPerPage);
-      
-      if($records->isEmpty() === false){
+
+      if ($records->isEmpty() === false) {
         GlobalCache::set($key, $records);
       }
     }
