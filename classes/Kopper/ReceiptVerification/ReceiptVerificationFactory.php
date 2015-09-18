fcb1070c5f93ea88f5e5d788dffcccd4be5e2ad7
Ben Connito <benconnito@gmail.com>
2015-09-18 14:21:13 -0400

    add google and amazon to verification factory

--- classes/Kopper/ReceiptVerification/ReceiptVerificationFactory.php
+++ classes/Kopper/ReceiptVerification/ReceiptVerificationFactory.php
@@ -10,6 +10,10 @@ class ReceiptVerificationFactory {
     switch ($store) {
       case AppleReceiptVerification::STORE_NAME:
         return new AppleReceiptVerification();
+      case GoogleReceiptVerification::STORE_NAME:
+        return new GoogleReceiptVerification();
+      case AmazonReceiptVerification::STORE_NAME:
+        return new AmazonReceiptVerification();
       default:
         throw new Exception("invalid store ($store)");
     }
