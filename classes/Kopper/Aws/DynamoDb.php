333da6e9fcbde56bf44e455726638269e54f73de
Ben Connito <benconnito@gmail.com>
2015-10-30 10:08:21 -0400

    use paginators in dynamo raw query and scan in dynamo

--- classes/Kopper/Aws/DynamoDb.php
+++ classes/Kopper/Aws/DynamoDb.php
@@ -10,206 +10,216 @@ use Kopper\Environment;
 
 class DynamoDb extends DbClient {
 
-  const DAY_FORMAT = 'Y_m_d';
-  const ALL_TABLES_KEY = 'dynamo-tables';
+	const DAY_FORMAT = 'Y_m_d';
+	const ALL_TABLES_KEY = 'dynamo-tables';
 
-  public function __construct($config = array()) {
-    parent::__construct($config);
+	public function __construct($config = array()) {
+		parent::__construct($config);
 
-    $this->client = new DynamoDbClient($this->config);
-  }
+		$this->client = new DynamoDbClient($this->config);
+	}
 
-  protected function isValidKey(array $key) {
-    return true;
-  }
+	protected function isValidKey(array $key) {
+		return true;
+	}
 
-  public function getItem($tableName, array $key) {
-    $marshaler = new Marshaler();
+	public function getItem($tableName, array $key) {
+		$marshaler = new Marshaler();
 
-    $result = $this->client->getItem(array(
-      'ConsistentRead' => true,
-      'TableName' => $this->getRealEnvName($tableName),
-      'Key' => $marshaler->marshalItem($key)
-    ));
+		$result = $this->client->getItem(array(
+			'ConsistentRead' => true,
+			'TableName' => $this->getRealEnvName($tableName),
+			'Key' => $marshaler->marshalItem($key)
+		));
 
-    if (isset($result['Item']) === true) {
-      return $marshaler->unmarshalItem($result['Item']);
-    } else {
-      return null;
-    }
-  }
+		if (isset($result['Item']) === true) {
+			return $marshaler->unmarshalItem($result['Item']);
+		} else {
+			return null;
+		}
+	}
 
-  public function putItem($tableName, array $item, $condition = null, $expressionValuesMap = null) {
-    $marshaler = new Marshaler();
+	public function putItem($tableName, array $item, $condition = null, $expressionValuesMap = null) {
+		$marshaler = new Marshaler();
 
-    $params = array(
-      'TableName' => $this->getRealEnvName($tableName),
-      'Item' => $marshaler->marshalItem($item)
-    );
+		$params = array(
+			'TableName' => $this->getRealEnvName($tableName),
+			'Item' => $marshaler->marshalItem($item)
+		);
 
-    if (empty($condition) === false) {
-      $params['ConditionExpression'] = $condition;
+		if (empty($condition) === false) {
+			$params['ConditionExpression'] = $condition;
 
-      if (empty($expressionValuesMap) === false) {
-        $params['ExpressionAttributeValues'] = $expressionValuesMap;
-      }
-    }
+			if (empty($expressionValuesMap) === false) {
+				$params['ExpressionAttributeValues'] = $expressionValuesMap;
+			}
+		}
 
-    $this->client->putItem($params);
-  }
+		$this->client->putItem($params);
+	}
 
-  public function updateItem($tableName, array $key, $updateExpression, $expressionValuesMap = null) {
-    $marshaler = new Marshaler();
+	public function updateItem($tableName, array $key, $updateExpression, $expressionValuesMap = null) {
+		$marshaler = new Marshaler();
 
-    $params = array(
-      'TableName' => $this->getRealEnvName($tableName),
-      'Key' => $marshaler->marshalItem($key),
-      'ReturnValues' => 'UPDATED_NEW'
-    );
+		$params = array(
+			'TableName' => $this->getRealEnvName($tableName),
+			'Key' => $marshaler->marshalItem($key),
+			'ReturnValues' => 'UPDATED_NEW'
+		);
 
-    $params['UpdateExpression'] = $updateExpression;
+		$params['UpdateExpression'] = $updateExpression;
 
-    if (empty($expressionValuesMap) === false) {
-      $params['ExpressionAttributeValues'] = $expressionValuesMap;
-    }
+		if (empty($expressionValuesMap) === false) {
+			$params['ExpressionAttributeValues'] = $expressionValuesMap;
+		}
 
-    $result = $this->client->updateItem($params);
+		$result = $this->client->updateItem($params);
 
-    if (isset($result['Attributes']) === true) {
-      return $marshaler->unmarshalItem($result['Attributes']);
-    } else {
-      return null;
-    }
-  }
+		if (isset($result['Attributes']) === true) {
+			return $marshaler->unmarshalItem($result['Attributes']);
+		} else {
+			return null;
+		}
+	}
 
-  public function deleteItem($tableName, array $key) {
-    $marshaler = new Marshaler();
+	public function deleteItem($tableName, array $key) {
+		$marshaler = new Marshaler();
 
-    $this->client->deleteItem(array(
-      'TableName' => $this->getRealEnvName($tableName),
-      'Key' => $marshaler->marshalItem($key)
-    ));
-  }
+		$this->client->deleteItem(array(
+			'TableName' => $this->getRealEnvName($tableName),
+			'Key' => $marshaler->marshalItem($key)
+		));
+	}
 
-  public function listTables($clearCache = false) {
-    $tables = LocalCache::get(self::ALL_TABLES_KEY);
+	public function listTables($clearCache = false) {
+		$tables = LocalCache::get(self::ALL_TABLES_KEY);
 
-    if ($tables === false || $clearCache == true) {
-      $result = $this->client->listTables();
+		if ($tables === false || $clearCache == true) {
+			$result = $this->client->listTables();
 
-      $tables = $result['TableNames'];
-      LocalCache::set(self::ALL_TABLES_KEY, $tables);
-    }
+			$tables = $result['TableNames'];
+			LocalCache::set(self::ALL_TABLES_KEY, $tables);
+		}
 
-    return $tables;
-  }
+		return $tables;
+	}
 
-  public function createTable($tableName, array $schema = null) {
-    $fullTableName = $this->getRealEnvName($tableName);
-    $schema['TableName'] = $fullTableName;
+	public function createTable($tableName, array $schema = null) {
+		$fullTableName = $this->getRealEnvName($tableName);
+		$schema['TableName'] = $fullTableName;
 
-    if (empty($schema['ProvisionedThroughput']) == true) {
-      if (APPLICATION_ENV == Environment::PRODUCTION) {
-        $schema['ProvisionedThroughput'] = array(
-          'ReadCapacityUnits' => 60,
-          'WriteCapacityUnits' => 60
-        );
-      } else {
-        $schema['ProvisionedThroughput'] = array(
-          'ReadCapacityUnits' => 5,
-          'WriteCapacityUnits' => 5
-        );
-      }
-    }
+		if (empty($schema['ProvisionedThroughput']) == true) {
+			if (APPLICATION_ENV == Environment::PRODUCTION) {
+				$schema['ProvisionedThroughput'] = array(
+					'ReadCapacityUnits' => 60,
+					'WriteCapacityUnits' => 60
+				);
+			} else {
+				$schema['ProvisionedThroughput'] = array(
+					'ReadCapacityUnits' => 5,
+					'WriteCapacityUnits' => 5
+				);
+			}
+		}
 
-    try {
-      $this->client->createTable($schema);
-    } catch (ResourceInUseException $e) {
-      //already created wait for table existis
-    }
+		try {
+			$this->client->createTable($schema);
+		} catch (ResourceInUseException $e) {
+			//already created wait for table existis
+		}
 
-    $this->client->waitUntil('TableExists', array('TableName' => $fullTableName));
+		$this->client->waitUntil('TableExists', array('TableName' => $fullTableName));
 
-    $this->listTables(true);
-  }
+		$this->listTables(true);
+	}
 
-  public function updateThroughput($tableName, $read, $write) {
-    $this->client->updateTable(array(
-      'TableName' => $this->getRealEnvName($tableName),
-      'ProvisionedThroughput' => array(
-        'ReadCapacityUnits' => $read,
-        'WriteCapacityUnits' => $write
-      )
-    ));
-  }
+	public function updateThroughput($tableName, $read, $write) {
+		$this->client->updateTable(array(
+			'TableName' => $this->getRealEnvName($tableName),
+			'ProvisionedThroughput' => array(
+				'ReadCapacityUnits' => $read,
+				'WriteCapacityUnits' => $write
+			)
+		));
+	}
 
-  public function deleteTable($tableName) {
-    $this->client->deleteTable(array('TableName' => $this->getRealEnvName($tableName)));    
-    $this->client->waitUntil('TableNotExists', array('TableName' => $this->getRealEnvName($tableName)));
+	public function deleteTable($tableName) {
+		$this->client->deleteTable(array('TableName' => $this->getRealEnvName($tableName)));
+		$this->client->waitUntil('TableNotExists', array('TableName' => $this->getRealEnvName($tableName)));
 
-    $this->listTables(true);
-  }
+		$this->listTables(true);
+	}
 
-  public function count($tableName, array $conditions = null) {
-    $params = array(
-      'TableName' => $this->getRealEnvName($tableName),
-      'Select', 'COUNT'
-    );
+	public function count($tableName, array $conditions = null) {
+		$params = array(
+			'TableName' => $this->getRealEnvName($tableName),
+			'Select', 'COUNT'
+		);
 
-    if (empty($conditions) == false) {
-      $params['ScanFilter'] = $conditions;
-    }
+		if (empty($conditions) == false) {
+			$params['ScanFilter'] = $conditions;
+		}
 
-    $result = $this->client->scan($params);
+		$result = $this->client->scan($params);
 
-    return $result['Count'];
-  }
+		return $result['Count'];
+	}
 
-  public function getAll($tableName, array $fields = null) {
-    $params = array(
-      'TableName' => $this->getRealEnvName($tableName)
-    );
+	public function getAll($tableName, array $fields = null) {
+		$params = array(
+			'TableName' => $this->getRealEnvName($tableName)
+		);
 
-    if (empty($fields) == false) {
-      $params['Select'] = 'SPECIFIC_ATTRIBUTES';
-      $params['AttributesToGet'] = $fields;
-    }
+		if (empty($fields) == false) {
+			$params['Select'] = 'SPECIFIC_ATTRIBUTES';
+			$params['AttributesToGet'] = $fields;
+		}
 
-    return $this->client->getScanIterator($params);
-  }
+		return $this->client->getScanPaginator($params);
+	}
 
-  public function scan($tableName, array $conditions = null, array $fields = null) {
-    $params = array(
-      'TableName' => $this->getRealEnvName($tableName)
-    );
+	public function scan($tableName, array $conditions = null, array $fields = null) {
+		$params = array(
+			'TableName' => $this->getRealEnvName($tableName)
+		);
 
-    if (empty($fields) == false) {
-      $params['Select'] = 'SPECIFIC_ATTRIBUTES';
-      $params['AttributesToGet'] = $fields;
-    }
+		if (empty($fields) == false) {
+			$params['Select'] = 'SPECIFIC_ATTRIBUTES';
+			$params['AttributesToGet'] = $fields;
+		}
 
-    if (empty($conditions) == false) {
-      $params['ScanFilter'] = $conditions;
-    }
+		if (empty($conditions) == false) {
+			$params['ScanFilter'] = $conditions;
+		}
 
-    return $this->client->getScanIterator($params);
-  }
+		return $this->client->getScanPaginator($params);
+	}
 
-  public function query($tableName, array $conditions = null, array $fields = null) {
-    $params = array(
-      'TableName' => $this->getRealEnvName($tableName)
-    );
+	public function query($tableName, array $conditions = null, array $fields = null) {
+		$params = array(
+			'TableName' => $this->getRealEnvName($tableName)
+		);
 
-    if (empty($fields) == false) {
-      $params['Select'] = 'SPECIFIC_ATTRIBUTES';
-      $params['AttributesToGet'] = $fields;
-    }
+		if (empty($fields) == false) {
+			$params['Select'] = 'SPECIFIC_ATTRIBUTES';
+			$params['AttributesToGet'] = $fields;
+		}
 
-    if (empty($conditions) == false) {
-      $params['KeyConditions'] = $conditions;
-    }
+		if (empty($conditions) == false) {
+			$params['KeyConditions'] = $conditions;
+		}
 
-    return $this->client->getQueryIterator($params);
-  }
+		return $this->client->getQueryPaginator($params);
+	}
+
+	public function rawScan($tableName, array $params) {
+		$params['TableName'] = $this->getRealEnvName($tableName);
+		return $this->client->getScanPaginator($params);
+	}
+
+	public function rawQuery($tableName, array $params) {
+		$params['TableName'] = $this->getRealEnvName($tableName);
+		return $this->client->getQueryPaginator($params);
+	}
 
 }
