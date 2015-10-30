00513554bf6cdb92d4736b0acdfd2794abf9e362
Ben Connito <benconnito@gmail.com>
2015-10-30 10:18:02 -0400

    no need for fields

--- classes/Kopper/Aws/DbClient.php
+++ classes/Kopper/Aws/DbClient.php
@@ -6,34 +6,34 @@ use Exception;
 
 abstract class DbClient extends AwsClient {
 
-  abstract protected function isValidKey(array $key);
+	abstract protected function isValidKey(array $key);
 
-  protected function invalidKey(array $key) {
-    throw new Exception('invalid key - ' . json_encode($key));
-  }
+	protected function invalidKey(array $key) {
+		throw new Exception('invalid key - ' . json_encode($key));
+	}
 
-  abstract public function getItem($tableName, array $key);
+	abstract public function getItem($tableName, array $key);
 
-  abstract public function putItem($tableName, array $item);
+	abstract public function putItem($tableName, array $item);
 
-  abstract public function updateItem($tableName, array $key, $item);
+	abstract public function updateItem($tableName, array $key, $item);
 
-  abstract public function deleteItem($tableName, array $key);
+	abstract public function deleteItem($tableName, array $key);
 
-  abstract public function listTables($clearCache = false);
+	abstract public function listTables($clearCache = false);
 
-  abstract public function createTable($tableName, array $schema = null);
+	abstract public function createTable($tableName, array $schema = null);
 
-  public function createTableIfNotExist($tableName, array $schema = null) {
-    $tables = $this->listTables();
-    $fullTableName = $this->getRealEnvName($tableName);
+	public function createTableIfNotExist($tableName, array $schema = null) {
+		$tables = $this->listTables();
+		$fullTableName = $this->getRealEnvName($tableName);
 
-    if (in_array($fullTableName, $tables) == false) {
-      $this->createTable($tableName, $schema);
-    }
-  }
+		if (in_array($fullTableName, $tables) == false) {
+			$this->createTable($tableName, $schema);
+		}
+	}
 
-  abstract public function deleteTable($tableName);
+	abstract public function deleteTable($tableName);
 
-  abstract public function getAll($tableName, array $fields = null);
+	abstract public function getAll($tableName);
 }
--- classes/Kopper/Aws/SimpleDb.php
+++ classes/Kopper/Aws/SimpleDb.php
@@ -7,158 +7,158 @@ use Kopper\Cache\LocalCache;
 
 class SimpleDb extends DbClient {
 
-  const NAME = 'Name';
-  const ALL_TABLES_KEY = 'simpledb-tables';
+	const NAME = 'Name';
+	const ALL_TABLES_KEY = 'simpledb-tables';
 
-  public function __construct($config = array()) {
-    parent::__construct($config);
+	public function __construct($config = array()) {
+		parent::__construct($config);
 
-    $this->client = new SimpleDbClient($this->config);
-  }
+		$this->client = new SimpleDbClient($this->config);
+	}
 
-  protected function isValidKey(array $key) {
-    return (empty($key[self::NAME]) === false && preg_match('/^[a-zA-Z0-9_]+$/', $key[self::NAME]) === 1);
-  }
+	protected function isValidKey(array $key) {
+		return (empty($key[self::NAME]) === false && preg_match('/^[a-zA-Z0-9_]+$/', $key[self::NAME]) === 1);
+	}
 
-  protected function getKey(array $key) {
-    if ($this->isValidKey($key) === true) {
-      return (string) $key[self::NAME];
-    } else {
-      $this->invalidKey($key);
-    }
-  }
+	protected function getKey(array $key) {
+		if ($this->isValidKey($key) === true) {
+			return (string) $key[self::NAME];
+		} else {
+			$this->invalidKey($key);
+		}
+	}
 
-  public function getItem($tableName, array $key) {
-    $result = $this->client->getAttributes(array(
-      'DomainName' => $this->getRealEnvName($tableName),
-      'ItemName' => $this->getKey($key)
-    ));
+	public function getItem($tableName, array $key) {
+		$result = $this->client->getAttributes(array(
+			'DomainName' => $this->getRealEnvName($tableName),
+			'ItemName' => $this->getKey($key)
+		));
 
-    return $result['Attributes'];
-  }
+		return $result['Attributes'];
+	}
 
-  public function putItem($tableName, array $item) {
-    $this->client->putAttributes(array(
-      'DomainName' => $this->getRealEnvName($tableName),
-      'ItemName' => $this->getKey($item),
-      'Attributes' => $item['Attributes']
-    ));
-  }
+	public function putItem($tableName, array $item) {
+		$this->client->putAttributes(array(
+			'DomainName' => $this->getRealEnvName($tableName),
+			'ItemName' => $this->getKey($item),
+			'Attributes' => $item['Attributes']
+		));
+	}
 
-  public function updateItem($tableName, array $key, $item) {
-    $this->putItem($tableName, $item);
-  }
+	public function updateItem($tableName, array $key, $item) {
+		$this->putItem($tableName, $item);
+	}
 
-  public function deleteItem($tableName, array $key) {
-    $this->client->deleteAttributes(array(
-      'DomainName' => $this->getRealEnvName($tableName),
-      'ItemName' => $this->getKey($key)
-    ));
-  }
+	public function deleteItem($tableName, array $key) {
+		$this->client->deleteAttributes(array(
+			'DomainName' => $this->getRealEnvName($tableName),
+			'ItemName' => $this->getKey($key)
+		));
+	}
 
-  public function listTables($clearCache = false) {
-    $tables = LocalCache::get(self::ALL_TABLES_KEY);
+	public function listTables($clearCache = false) {
+		$tables = LocalCache::get(self::ALL_TABLES_KEY);
 
-    if ($tables === false || $clearCache == true) {
-      $nextToken = null;
-      $tables = array();
+		if ($tables === false || $clearCache == true) {
+			$nextToken = null;
+			$tables = array();
 
-      do {
-        if (empty($nextToken) == false) {
-          $result = $this->client->listDomains(array(
-            'NextToken' => $nextToken
-          ));
-        } else {
-          $result = $this->client->listDomains();
-        }
+			do {
+				if (empty($nextToken) == false) {
+					$result = $this->client->listDomains(array(
+						'NextToken' => $nextToken
+					));
+				} else {
+					$result = $this->client->listDomains();
+				}
 
-        $tables = array_merge($tables, $result['DomainNames']);
-        $nextToken = $result['NextToken'];
-      } while (empty($nextToken) == false);
+				$tables = array_merge($tables, $result['DomainNames']);
+				$nextToken = $result['NextToken'];
+			} while (empty($nextToken) == false);
 
-      LocalCache::set(self::ALL_TABLES_KEY, $tables);
-    }
+			LocalCache::set(self::ALL_TABLES_KEY, $tables);
+		}
 
-    return $tables;
-  }
+		return $tables;
+	}
 
-  public function createTable($tableName, array $schema = null) {
-    $tables = $this->listTables();
+	public function createTable($tableName, array $schema = null) {
+		$tables = $this->listTables();
 
-    $table = $this->getRealEnvName($tableName);
+		$table = $this->getRealEnvName($tableName);
 
-    if (in_array($table, $tables) === false) {
-      $this->client->createDomain(array(
-        'DomainName' => $table,
-      ));
+		if (in_array($table, $tables) === false) {
+			$this->client->createDomain(array(
+				'DomainName' => $table,
+			));
 
-      $this->listTables(true);
-    }
-  }
+			$this->listTables(true);
+		}
+	}
 
-  public function deleteTable($tableName) {
-    $this->client->deleteDomain(array(
-      'DomainName' => $this->getRealEnvName($tableName),
-    ));
+	public function deleteTable($tableName) {
+		$this->client->deleteDomain(array(
+			'DomainName' => $this->getRealEnvName($tableName),
+		));
 
-    $this->listTables(true);
-  }
+		$this->listTables(true);
+	}
 
-  public function getAll($tableName, array $fields = null) {
-    $tableName = $this->getRealEnvName($tableName);
+	public function getAll($tableName) {
+		$tableName = $this->getRealEnvName($tableName);
 
-    return $this->query("select * from $tableName");
-  }
+		return $this->query("select * from $tableName");
+	}
 
-  public function query($query, $limit = null, $nextToken = null) {
-    $records = array();
+	public function query($query, $limit = null, $nextToken = null) {
+		$records = array();
 
-    if (empty($limit) === false) {
-      $query .= " limit $limit";
-    }
+		if (empty($limit) === false) {
+			$query .= " limit $limit";
+		}
 
-    $done = false;
+		$done = false;
 
-    do {
-      if (empty($limit) === true || count($records) < $limit) {
-        if (isset($nextToken)) {
-          $result = $this->client->select(array(
-            'SelectExpression' => $query,
-            'NextToken' => $nextToken
-          ));
-        } else {
-          $result = $this->client->select(array(
-            'SelectExpression' => $query
-          ));
-        }
+		do {
+			if (empty($limit) === true || count($records) < $limit) {
+				if (isset($nextToken)) {
+					$result = $this->client->select(array(
+						'SelectExpression' => $query,
+						'NextToken' => $nextToken
+					));
+				} else {
+					$result = $this->client->select(array(
+						'SelectExpression' => $query
+					));
+				}
 
-        if (empty($result['Items']) === false) {
-          $records = array_merge($records, $result['Items']);
-        }
+				if (empty($result['Items']) === false) {
+					$records = array_merge($records, $result['Items']);
+				}
 
-        $nextToken = isset($result['NextToken']) ? $result['NextToken'] : null;
+				$nextToken = isset($result['NextToken']) ? $result['NextToken'] : null;
 
-        if (empty($nextToken) === true) {
-          $done = true;
-        }
-      } else {
-        $done = true;
-      }
-    } while ($done === false);
+				if (empty($nextToken) === true) {
+					$done = true;
+				}
+			} else {
+				$done = true;
+			}
+		} while ($done === false);
 
-    return (object) array(
-        'records' => $records,
-        'nextToken' => $nextToken
-    );
-  }
+		return (object) array(
+				'records' => $records,
+				'nextToken' => $nextToken
+		);
+	}
 
-  public function batchPutItems($tableName, array $items) {
-    while (count($items) > 0) {
-      $this->client->batchPutAttributes(array(
-        'DomainName' => $this->getRealEnvName($tableName),
-        'Items' => array_splice($items, 0, 25)
-      ));
-    }
-  }
+	public function batchPutItems($tableName, array $items) {
+		while (count($items) > 0) {
+			$this->client->batchPutAttributes(array(
+				'DomainName' => $this->getRealEnvName($tableName),
+				'Items' => array_splice($items, 0, 25)
+			));
+		}
+	}
 
 }
